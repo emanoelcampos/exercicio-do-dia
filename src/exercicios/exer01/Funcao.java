9ab3a87ecd4f17696a4021d1d1c36d859b8c3bc5
emanoelcampos <emanoel.cv@gmail.com>
2022-08-20 17:25:27 -0300

    exercicio funcao

---
+++ src/exercicios/exer01/Funcao.java
@@ -0,0 +1,24 @@
+package exercicios.exer01;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Funcao {
+
+    public static void contaRepedido(int[] numeros){
+        Map<Integer, Integer> contador = new HashMap<>();
+        for(int repetido : numeros){
+            if(!contador.containsKey(repetido))
+                contador.put(repetido, 0);
+            contador.put(repetido, contador.get(repetido) +1);
+        }
+        for (Map.Entry<Integer, Integer> entry : contador.entrySet()) {
+            if(entry.getValue() > 1)
+                System.out.printf("\nNúmero %s : %d vezes", entry.getKey(), entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        Funcao.contaRepedido(new int[] {1, 7, 9, 7, 4, 3, 9, 4, 3, 7});
+    }
+}
